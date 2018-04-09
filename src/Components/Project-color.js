417afc11838dab7162e17072ddfa039548644da3
HaiYen <Fiststar1995@gmail.com>
2018-04-09 18:00:14 +0700

    up code

--- src/Components/Project-color.js
+++ src/Components/Project-color.js
@@ -3,34 +3,42 @@ import Projectcolora from './Project_color_a';
 import Projectcolorb from './Project_color_b';
 
 class Projectcolor extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      color:'red',
+      fontSize:16
+    };
+  }
+  
   render(){
     return(
-            <div className="row" id="color-project">
-              <div className="col-sm-12 col-xs-12">
-                <hr />
-              </div>
-              <div className="col-sm-12 col-xs-12 text-center">
-                <h1>Project text change color</h1>
-              </div>
-              <div className="col-sm-6 col-xs-12">
-                <Projectcolora></Projectcolora>
-              </div>
-              <div className="col-sm-6 col-xs-12">
-                <Projectcolorb></Projectcolorb>
-              </div>
-              <div className="col-sm-12 col-xs-12">
-                <div className="row">
-                  <div className="col-sm-6 col-xs-12">
-                    <h4><b>Màu: Đỏ</b></h4>
-                    <h4><b>Font-size: 12px</b></h4>
-                  </div>
-                  <div className="col-sm-6 col-xs-12">
-                      <input className="control-form" placeholder="Trần Hải Yến" />
-                  </div>
-                </div>
-              </div>
+      <div className="row" id="color-project">
+        <div className="col-sm-12 col-xs-12">
+          <hr />
+        </div>
+        <div className="col-sm-12 col-xs-12 text-center">
+          <h1>Project text change color</h1>
+        </div>
+        <div className="col-sm-6 col-xs-12">
+          <Projectcolora color_prop={this.state.color}></Projectcolora>
+        </div>
+        <div className="col-sm-6 col-xs-12">
+          <Projectcolorb></Projectcolorb>
+        </div>
+        <div className="col-sm-12 col-xs-12">
+          <div className="row">
+            <div className="col-sm-6 col-xs-12">
+              <h4><b>Màu: Đỏ</b></h4>
+              <h4><b>Font-size: 12px</b></h4>
             </div>
-            );
+            <div className="col-sm-6 col-xs-12">
+                <input className="form-control" placeholder="Trần Hải Yến" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 };
 export default Projectcolor;
--- src/Components/Project_color_a.js
+++ src/Components/Project_color_a.js
@@ -1,20 +1,39 @@
 import React, {Component} from 'react';
 class Projectcolora extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      colors:['red', 'green', 'blue', 'yellow', 'pink']
+    };
+  };
+  showColor(color){
+    return {
+            backgroundColor:color
+          };
+  };
   render(){
+    var elementcolor = this.state.colors.map((color,index) => {
+      return (
+              <li key={index} 
+                  style={ this.showColor(color)} 
+                  className={this.props.color_prop === color ? 'active' : ''}
+                 
+              >
+              </li>
+              ); 
+    });
     return(
-            <div className="panel panel-danger">
-              <div className="panel-heading">
-                <h5>Chọn màu</h5>
-              </div>
-              <div className="panel-body">
-                <ul className="color">
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
-              </div>
-            </div>
-            );
+      <div className="panel panel-danger">
+        <div className="panel-heading">
+          <h5>Chọn màu</h5>
+        </div>
+        <div className="panel-body">
+          <ul className="color">
+            {elementcolor}
+        </ul>
+        </div>
+      </div>
+    );
   }
 };
 export default Projectcolora;
